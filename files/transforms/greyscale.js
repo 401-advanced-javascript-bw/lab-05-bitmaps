c98eb3ecc055fa197dd0a96b6ebbd80f144d1bd7
Bonnie Wang <jcwang118@hotmail.com>
2019-05-18 21:30:57 -0700

    modularizing

---
+++ files/transforms/greyscale.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * Sample Transformer (greyscale)
+ * Would be called by Bitmap.transform('greyscale')
+ * Pro Tip: Use "pass by reference" to alter the bitmap's buffer in place so you don't have to pass it around ...
+ * @param bmp
+ */
+const transformGreyscale = bmp => {
+  console.log('Transforming bitmap into greyscale', bmp);
+
+  //TODO: Figure out a way to validate that the bmp instance is actually valid before trying to transform it
+  console.log(bmp.pixelArrayOffset, '###########');
+
+  //TODO: alter bmp to make the image greyscale ...
+};
+
+module.exports = transformGreyscale;
